6712be0e747554655196307acdf0cf0c4670e46b
ABDUL REHMAN <abdulrehmanars1996@gmail.com>
2026-04-18 23:56:19 +0500

    Add files via upload

---
+++ practice/app.js
@@ -0,0 +1,71 @@
+// 1. crete three variable: Name,age and is student. assign  them  appropriate  value print  them using console.log ()
+
+
+// let name = "saad"
+// let age = 25
+// let isStudent = true
+// console.log(`
+  
+// name ${name}
+// age ${age}
+// isStudent ${isStudent}
+
+
+
+// `)
+
+
+// 2. declare and ubdated variable
+
+
+// let age = 35
+// console.log(age)
+
+// age = 20
+// console.log(age);   // variable ubadted but const se bana huwa nahi huna chaye
+
+
+// 3. crete varaible count =1o. increase its value by 5 and then  print the result.
+
+// let count = 10
+
+// count = count + 10       // right jata hai left mein
+
+// count+= 5                //   is tarha bhe hu sakta hai
+
+// console.log(count);
+
+//  4. decalare two variable  a=5 and b=10. swab  their values using a third varaible and print the result.
+
+// let a=5
+// let b=10
+
+// let temp=a;        // temp ke andar 5 agya hai
+// a=b                // a=10  "A" ke andar abb ten aa chuka hai.
+// b=temp             // b=5  "b" ke andar abb ten aa chuka hai.            
+// console.log(a,b);
+
+// 2nd tarika karna ka
+
+// let a=5    
+// let b=10   
+// a=a+b      //5+10       // a=15
+// b=a-b      //15-10      //b=5
+// a=a-b      //15-5       //a=10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
