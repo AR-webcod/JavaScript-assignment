d5de5953c61badbd968898d2db29aebd8721c725
ABDUL REHMAN <abdulrehmanars1996@gmail.com>
2026-04-29 17:10:32 +0500

    updated
    this code practice in nested list and case code.

---
+++ if and else/nested list/app.js
@@ -0,0 +1,63 @@
+// let username = "admin"
+// let password = "1234"
+
+// let usernameinput = prompt("enter the user name")
+// let passwordinput = prompt("enter the password")
+
+
+// if (usernameinput == username) {
+//     let passwodinput = prompt("enter the passwod")
+
+//     if (passwodinput == passwrod) {
+//         console.log("login");
+
+//     }
+//     else {
+//         console.log("wrong passwrod");
+
+//     }
+// }
+
+// else {
+//     console.log("invalid user name");
+
+// }
+
+
+// if (usernameinput==username){
+//     console.log("login");
+    
+// }
+// else{
+//     console.log("invalid");
+    
+// }
+
+// if (passwordinput==password){
+//     console.log("login");
+    
+// }
+// else{
+//     console.log("invalid");
+    
+// }
+
+
+// SHPOING DICOUNT
+
+// let amount=prompt("enter the amount")
+
+// if(amount>1000){
+//     console.log("50% dicount");
+    
+// }
+
+// else if(amount>500){
+//     console.log("20% discount");
+    
+// }
+
+// else{
+//     console.log("no discount");
+    
+// }
