4488ad57f927eba21d76d024b46ce93ed3c27b7d
ABDUL REHMAN <abdulrehmanars1996@gmail.com>
2026-04-30 01:20:13 +0500

    Add files via upload

---
+++ if and else/LOPP/app.js
@@ -0,0 +1,80 @@
+// for (let i = 1; i <= 15; i++) {
+
+//     if (i == 10) {
+//         break           // break ka kaam loop se bhar chale jana or 10 pe rok jana
+//     }
+
+//     console.log(i, "welcome");   // isme ab ye hu ga ke last value matlp 10 number show nahi hu ga because breake phale laga huwa ha
+// }
+
+// for (let i = 1; i <= 15; i++) {
+//     if (i == 5 || i == 15) {
+//         continue                        // continue kia karta apki current value skip kar deta hai...
+//     }
+//     console.log(i, "welcome");
+
+// }
+
+
+// TYPES OF LOOP...
+// for and OF loop     
+
+// let s = "a.rehman"
+
+// for (let v of s) {       // OF kia karta varaible me jo value hai ek ek value "v" mein  add akrte rahe ga, wo bhe left se read karta hai....
+//     console.log(v);       // ' v '
+
+// }
+
+
+// let m = [1, 2, 3, 50,]
+
+// for (let v of m) {       
+//     console.log(v);      
+
+// }
+
+
+// let m = "abdul.rehman"
+
+// for (let v in m) {       
+//     console.log(v);      
+
+// }
+
+// let key = {
+//     'name': 'arehman',
+//     'username': 'saad'
+// }
+
+// for (let user in key) {                         // 'IN' key ya index dena ka kaam karta hai..
+//     console.log(user,key[user]);      
+
+// }
+
+// TABLE PRINT IN method for loop...
+// for (let i = 1; i <= 10; i++)
+//     console.log("2   X   " +   i   +    "  =  ", i * 2) ;
+
+// for (let i = 1; i <= 10; i++)
+//     console.log("3   X   " +   i   +    "  =  ", i * 3);
+
+// let sum = 0
+// for (let i = 1; i <= 10; i++){
+
+//     sum=sum+i
+// }
+
+// // ismein asal mein kia huwa sum jab + huwa 1 se answere aya 1 baki example nije diya huwa hai
+// // 0+1      1
+// // 1+2      3
+// // 3+3      6
+// // 6+4      10
+// // 10+5     15
+// // 15+6     21
+// // 21+7     28
+// // 28+8     36
+// // 36+9     45
+// // 45+10    55
+
+// console.log(sum);
